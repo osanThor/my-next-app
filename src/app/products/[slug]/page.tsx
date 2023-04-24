000acefcc004015598848834b1ab1f5bf1145200
junyoung <jadw9715@gmail.com>
2023-04-24 23:24:27 +0900

    fix. dynamic routing

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -7,4 +7,11 @@ const PantsPage = ({ params }: Props) => {
   return <div>{params.slug}</div>;
 };
 
+export function generateStateParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
+
 export default PantsPage;
