c51bce8cbe4855781dc079fad84cd1ddd93321cf
junyoung <jadw9715@gmail.com>
2023-04-24 23:54:53 +0900

    feat. next link

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -5,8 +5,8 @@ export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <div className={styles.nav}>
-        <Link href="/products/여성옷">여성옷</Link>
-        <Link href="/products/남성옷">남성옷</Link>
+        <Link href="/products/woman">여성옷</Link>
+        <Link href="/products/man">남성옷</Link>
       </div>
       <section className={styles.section}>{children}</section>
     </div>
