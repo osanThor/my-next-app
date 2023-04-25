beac988e30a576e031947118947b81d972ee89b4
junyoung <jadw9715@gmail.com>
2023-04-25 22:40:30 +0900

    feat.server component and client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import os from "os";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  console.log("안녕");
+  console.log(os.hostname());
+
   return <h1>홈페이지!</h1>;
 }
